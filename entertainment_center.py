5cd29e98b404bc23671edead195eee447dbc7b0f
wala602 <34238726+wala602@users.noreply.github.com>
2017-12-05 16:43:08 +0300

    Add files via upload

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,35 +1,38 @@
 import media
 import fresh_tomatoes
 
+# Creation of the Movie instances
+
 inception = media.Movie('Inception',
                        'A dream within a dream',
-                       'http://i.dailymail.co.uk/i/pix/2012/01/21/article-0-11644C10000005DC-276_634x463.jpg',
+                       'http://i.dailymail.co.uk/i/pix/2012/01/21/article-0-11644C10000005DC-276_634x463.jpg',  # NOQA
                        'https://www.youtube.com/watch?v=66TuSJo4dZM')
 
 avengers = media.Movie('Avengers',
                      'A story of a Marine on an alien planet',
-                     'https://orig00.deviantart.net/6be0/f/2011/004/6/f/__the_avengers___poster_2_by_themadbutcher-d36eop9.jpg',
+                     'https://orig00.deviantart.net/6be0/f/2011/004/6/f/__the_avengers___poster_2_by_themadbutcher-d36eop9.jpg',  # NOQA
                      'https://www.youtube.com/watch?v=eOrNdBpGMv8')
 
 die_hard = media.Movie('Die Hard',
                       'The story of John Mcclain chasing down bad guys',
-                      'https://images-na.ssl-images-amazon.com/images/I/51l7e1ENZvL._SY300_.jpg',
-                      'http://c8.alamy.com/comp/EFAW6Y/bruce-willis-poster-die-hard-with-a-vengeance-die-hard-3-1995-EFAW6Y.jpg')
+                      'https://images-na.ssl-images-amazon.com/images/I/51l7e1ENZvL._SY300_.jpg',  # NOQA
+                      'http://c8.alamy.com/comp/EFAW6Y/bruce-willis-poster-die-hard-with-a-vengeance-die-hard-3-1995-EFAW6Y.jpg')  # NOQA
 
 phonebooth = media.Movie('Phonebooth',
                       'The story of a guy in a phonebooth',
-                      'http://www.media4.hw-static.com/wp-content/uploads/phone-booth-movie-still-movie-poster_1711570-304x400.jpeg',
+                      'http://www.media4.hw-static.com/wp-content/uploads/phone-booth-movie-still-movie-poster_1711570-304x400.jpeg',  # NOQA
                       'https://www.youtube.com/watch?v=p07lBCfC2q8')
 
-narnia = media.Movie('The Chronicles of Narnia: The Lion, the Witch and the Wardrobe',
+narnia = media.Movie('The Chronicles of Narnia: The Lion, the Witch and the Wardrobe',  # NOQA
                      'A story of four kids who adventure in a mystical world',
-                     'https://images-na.ssl-images-amazon.com/images/M/MV5BMTc0NTUwMTU5OV5BMl5BanBnXkFtZTcwNjAwNzQzMw@@._V1_UX182_CR0,0,182,268_AL_.jpg',
+                     'https://images-na.ssl-images-amazon.com/images/M/MV5BMTc0NTUwMTU5OV5BMl5BanBnXkFtZTcwNjAwNzQzMw@@._V1_UX182_CR0,0,182,268_AL_.jpg',  # NOQA
                      'https://www.youtube.com/watch?v=pYcGFLgJ8Uo')
 
 harry_potter = media.Movie('Harry Potter and the Deathly Hallows',
                           'The start of the end',
-                          'https://images-na.ssl-images-amazon.com/images/I/51lbV87oWwL._SY300_.jpg',
+                          'https://images-na.ssl-images-amazon.com/images/I/51lbV87oWwL._SY300_.jpg',  # NOQA
                           'https://www.youtube.com/watch?v=9hXH0Ackz6w')
-
-movies = [inception,avengers,die_hard,phonebooth,narnia,harry_potter]
+# Creation of the list "movies" that will have all the created Movie instances
+movies = [inception, avengers, die_hard, phonebooth, narnia, harry_potter]
+# Calling the open_movies_page function with "movies" as the input
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,5 +1,9 @@
 import webbrowser
+
+
 class Movie():
+    """class Movie is class to create instances of movies, which has four
+    attributes: title, story, poster, trailer"""
     def __init__(self, m_title, m_story, m_poster, m_trailer):
         self.title = m_title
         self.story = m_story
@@ -7,6 +11,6 @@ class Movie():
         self.trailer = m_trailer
 
     def show_trailer(self):
+        """This function opens the web browser and opens the trailer for
+        the given movie instance"""
         webbrowser.open(self.trailer)
-        
-
